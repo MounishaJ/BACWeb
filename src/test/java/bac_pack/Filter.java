1ac32824b41cbfe7b45e08057a5ba1d4b517b0a0
mounisha <mounisha.resileo@gmail.com>
2019-08-30 17:57:04 +0530

    30-8-19

--- src/test/java/bac_pack/Filter.java
+++ src/test/java/bac_pack/Filter.java
@@ -1,12 +1,9 @@
 package bac_pack;
 
-import static org.testng.Assert.assertTrue;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,20 +12,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import pageObjects.CustomersPage;
 import resources.Base;
 
-public class single_filter extends Base {
-	
-		//23-8-2019 and 26-8-2019
-	
-	
-	List<String> Actual = new ArrayList<String>();
+public class Filter extends Base{
+
+	List<String> Filterlist = new ArrayList<String>();
 	 public static Logger log = LogManager.getLogger(Login_Functionality.class.getName());
 	@BeforeTest
 	public void initialize() throws IOException, InterruptedException {
@@ -49,19 +41,19 @@ public class single_filter extends Base {
 		Assert.assertEquals(p.customer_Tab().getText(), "Customers");
 	log.info("verifiled login page");
 	}
-	
+
 	@Test
 	public void singlefilter() throws InterruptedException
 		{
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[@class='add icon-plus']")).click();
 		String label=driver.findElement(By.xpath("//div[@class='fil_heading']")).getText();
-		Thread.sleep(2000);
-
-		Select registerstatus=new Select(driver.findElement(By.xpath("//select[@id='filter_type']"))); 
-		registerstatus.selectByVisibleText("Registered");
-	
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		 Select dropdown=new Select(driver.findElement(By.xpath("//select[@id='filter_type']")));
+		 
+		 dropdown.selectByVisibleText("Registered");
+		
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@id='filter_del_submit']")).click();
 		log.info("Successfully applied the Register status filter");
 		
@@ -71,7 +63,7 @@ public class single_filter extends Base {
 		for(int i=0;i<columncount;i++)
 		{
 			
-		//System.out.println("getattribute"+i+" "+driver.findElements(By.xpath("//thead/tr/th")).get(i).getText());
+		
 		String name=driver.findElements(By.xpath("//thead/tr/th")).get(i).getText();
 		//System.out.println(name);
 		if(name.equals(label))
@@ -86,17 +78,13 @@ public class single_filter extends Base {
 			List<WebElement> Rows = driver.findElements(By.xpath("//tr"));
 			int size=Rows.size(); //10
 			//System.out.println(size);
+			Thread.sleep(3000);
 			for (int j=1; j<size; j++) //10
 			  { 
 			//find the columns in specific row
-			//	Rows.get(j).findElements(By.xpath("//tr"+"["+ j +"]"+"//td"));
-		
 				
-			List<WebElement> Columns = Rows.get(j).findElements(By.xpath("//tr"+"["+ j +"]"+"//td"));
-			//System.out.println((Columns.get(i).getText())); 
-						
-			Actual.add(Columns.get(i).getText());
-						
+				 List<WebElement> Columns = Rows.get(j).findElements(By.xpath("//tr"+"["+ j +"]"+"//td"));
+			Filterlist.add(Columns.get(i).getText());
 			Iterator it = Columns.iterator(); // name is present
 			while (it.hasNext()) // next element is present or not
 			it.next();
@@ -122,34 +110,20 @@ public class single_filter extends Base {
 	    Integer countvalue=Integer.parseInt(count);
 		//System.out.println(countvalue);
 		
-		System.out.println("Array list size "+Actual.size());
-			 for(int i=0;i<Actual.size();i++)
+		System.out.println("Array list size "+Filterlist.size());
+			 for(int i=0;i<Filterlist.size();i++)
 				{
 				
 					//System.out.println(Actual.get(i));
 					
-					Assert.assertEquals(o,(Actual.get(i)));
+					Assert.assertEquals(o,(Filterlist.get(i)));
 					
 					
 				}
 				log.info("verified filtered records");
-		    Assert.assertTrue(countvalue.equals(Actual.size()));
+		    Assert.assertTrue(countvalue.equals(Filterlist.size()));
 			log.info("verified count values");
 								 
 		}
-			}
-/*
- * if(countvalue.equals(Actual.size())) { System.out.println("count matches"); }
- * else System.out.println("not matched count value");
- *//*
- * if(countvalue.equals(Actual.size())) {
- * System.out.println("count value matched"+" "+countvalue+" "+Actual.size()); }
- * else System.out.println("not mached"+countvalue);
- */	 
-				
-	/*
-	 * if(o.equals(Actual.get(i))) { System.out.println("matching"+
-	 * Actual.get(i)+" "+o); } else {
-	 * System.out.println("not matched"+Actual.get(i)+" "+o); }
-	 */								
-
+			
+}
